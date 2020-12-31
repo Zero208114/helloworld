806e9c0898224ebe01fc18c865cb21ed49cc4a10
Chen Xiyue <2364300382@qq.com>
2020-12-31 11:25:07 +0800

    Replace 'for' with 'while'.

--- sample_for/sample_for.cpp
+++ sample_for/sample_for.cpp
@@ -6,10 +6,11 @@ int main()
 	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
 		printf("%3d %6.1f\n", fahr, (5.0 / 9.0)*(fahr - 32));
 
-	printf("hello world\n");
-	printf("hello world\n");
-	printf("hello world\n");
-	printf("hello world\n");
-	printf("hello world\n");
+	int n;
+	n = 0;
+	while (n <= 4);
+		printf("hello world\n");
+		n = n + 1;
+
 }
 
