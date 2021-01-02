10642a0ffa729abfb27c984d70fd511fbd8a404e
Chen Xiyue <2364300382@qq.com>
2021-01-02 13:59:19 +0800

    exec 1-9 finished.

--- exec-01-09/exec-01-09.cpp
+++ exec-01-09/exec-01-09.cpp
@@ -3,8 +3,20 @@
 int main()
 {
 	int c;
+	int pre_char_is_space = 0;
 
 	while ((c = getchar()) != EOF) {
-		putchar(c);
+		if (c == ' ') {
+			if (pre_char_is_space == 0)
+			{
+				putchar(' ');
+				pre_char_is_space = 1;
+			}
+		}
+		else {
+			putchar(c);
+			pre_char_is_space = 0;
+
+		}
 	}
 }
