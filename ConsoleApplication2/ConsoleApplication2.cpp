3533ecd7168b6f3aeedb6ce14429f083d7de669c
Chen Xiyue <2364300382@qq.com>
2021-01-02 13:41:45 +0800

    fix bug

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,15 +2,16 @@
 
 int  main()
 {
-	int c, nl,nm,nc;
+	int c, nl, nm, nc;
 
-	nl = 0,nm=0,nc=0;
-	while ((c = getchar()) != EOF)
+	nl = 0, nm = 0, nc = 0;
+	while ((c = getchar()) != EOF) {
 		if (c == '\n')
 			++nl;
-	if (c == '\t')
-		++nm;
-	if (c == ' ')
-		++nc;
-	printf("%d\t%d\t%d\n ",nl,nm,nc);
+		if (c == '\t')
+			++nm;
+		if (c == ' ')
+			++nc;
+	}
+	printf("%d\t%d\t%d\n ", nl, nm, nc);
 }
