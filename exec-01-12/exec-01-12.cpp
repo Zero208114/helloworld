d6d8b41166bb8747b0030faa0f8ff261144f11d8
Chen Xiyue <2364300382@qq.com>
2021-01-02 14:43:21 +0800

    exec-01-12

---
+++ exec-01-12/exec-01-12.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int c;
+	int pre_char_is_space = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ') {
+			if (pre_char_is_space == 0)
+			{
+				putchar(' ');
+				pre_char_is_space = 1;
+			}
+		}
+		else {
+			putchar(c);
+			pre_char_is_space = 0;
+
+		}
+	}
+}
