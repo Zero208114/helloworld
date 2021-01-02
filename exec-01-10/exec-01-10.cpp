a2b6eae60c364cea3b033a6c2eded45daf596afc
Chen Xiyue <2364300382@qq.com>
2021-01-02 14:13:54 +0800

    exec-1-10 finished.

--- exec-01-10/exec-01-10.cpp
+++ exec-01-10/exec-01-10.cpp
@@ -5,6 +5,22 @@ int main()
 	int c;
 
 	while ((c = getchar()) != EOF) {
-		putchar(c);
+		if (c == '\t') {
+			putchar('\\');
+			putchar('t');
+		}
+		else if (c == '\b') {
+			putchar('\\');
+			putchar('b');
+		}
+		else if (c == '\\')
+		{
+			putchar('\\');
+			putchar('\\');
+		}
+		else {
+
+			putchar(c);
+		}
 	}
 }
